509830186f2304f49c06dd6a6ebb45ac7c172217
Capella87 <contact@capelladev.com>
2022-07-22 16:45:37 +0900

    Add feed app

--- djangogram/urls.py
+++ djangogram/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    # it need to be changed.
+    path('feed/', include('feed.urls')),
+    path('', RedirectView.as_view(url='feed/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
